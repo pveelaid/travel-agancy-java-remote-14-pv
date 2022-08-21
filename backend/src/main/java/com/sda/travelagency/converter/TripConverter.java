4e4e476bc1ca8d7856f56ee7c8f7697eb1bf805e
pveelaid <pveelaid@gmail.com>
2022-08-21 14:02:23 +0300

    [change] - converters in progress

--- backend/src/main/java/com/sda/travelagency/converter/TripConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/TripConverter.java
@@ -6,10 +6,36 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TripConverter implements Converter<Trip, TripDto> {
+
+    private final DestinationConverter destinationConverter;
+    private final SecurityRulesConverter securityRulesConverter;
+    private final HotelFacilitiesConverter hotelFacilitiesConverter;
+
+    public TripConverter(DestinationConverter destinationConverter, SecurityRulesConverter securityRulesConverter, HotelFacilitiesConverter hotelFacilitiesConverter) {
+        this.destinationConverter = destinationConverter;
+        this.securityRulesConverter = securityRulesConverter;
+        this.hotelFacilitiesConverter = hotelFacilitiesConverter;
+    }
+
     @Override
     public TripDto fromEntityToDto(Trip trip) {
+       var  destinationDto = destinationConverter.fromEntityToDto(trip.getDestination());
+       var securityRulesDto = securityRulesConverter.fromEntityToDto(trip.getSecurityRules());
+       var hotelFacilitiesDto = hotelFacilitiesConverter.fromEntityToDto(trip.getHotelFacilities());
 
-        return null;
+        return TripDto.builder()
+                .tripStartDate(trip.getTripStartDate())
+                .tripEndDate(trip.getTripEndDate())
+                .destination(destinationDto)
+                .cost(trip.getTripPrice().getCost().toString())
+                .cost(trip.getTripPrice().getCurrency())
+                .typeOfTransport(trip.getTypeOfTransport())
+                .securityRules(securityRulesDto)
+                .paymentType(trip.getPaymentType())
+                .mealType(trip.getMealType())
+                .hotelFacilities(hotelFacilitiesDto)
+                .photos(trip.getPhotos())
+                .build();
     }
 
     @Override

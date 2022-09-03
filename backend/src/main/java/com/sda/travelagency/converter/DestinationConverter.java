b2c7e618c934cd22cb2fd209507a5e206b0e46e7
pveelaid <pveelaid@gmail.com>
2022-09-03 11:00:50 +0300

    [change] - HotelFacilitiesConverter - ready

--- backend/src/main/java/com/sda/travelagency/converter/DestinationConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/DestinationConverter.java
@@ -5,21 +5,17 @@ import com.sda.travelagency.entity.Destination;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DestinationConverter implements Converter<Destination, DestinationDto>{
+public class DestinationConverter implements Converter<Destination, DestinationDto> {
 
     @Override
     public DestinationDto fromEntityToDto(Destination entity) {
 
-                return new DestinationDto(entity.getCountry(),
-                entity.getCity(),
-                entity.getHotelName());
+        return new DestinationDto(entity.getCountry(), entity.getCity(), entity.getHotelName());
     }
 
     @Override
     public Destination fromDtoToEntity(DestinationDto dto) {
 
-        return new Destination(dto.country(),
-                dto.city(),
-                dto.hotelName());
+        return new Destination(dto.country(), dto.city(), dto.hotelName());
     }
 }
--- backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
@@ -1,19 +1,56 @@
 package com.sda.travelagency.converter;
 
+import com.sda.travelagency.dto.AttractionDto;
 import com.sda.travelagency.dto.HotelFacilitiesDto;
+import com.sda.travelagency.entity.Attraction;
 import com.sda.travelagency.entity.HotelFacilities;
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class HotelFacilitiesConverter implements Converter<HotelFacilities, HotelFacilitiesDto> {
+
+    private final AttractionConverter attractionConverter;
+
+    public HotelFacilitiesConverter(AttractionConverter attractionConverter) {
+        this.attractionConverter = attractionConverter;
+    }
+
+
+
+    // till Java 8 implementation
+   /* @Override
+    public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
+        List<AttractionDto> attractionDtos = new ArrayList<>();
+        for (Attraction attraction: entity.getAttractions()) {
+            AttractionDto attractionDto = attractionConverter.fromEntityToDto(attraction);
+            attractionDtos.add(attractionDto);
+        }
+
+        return new HotelFacilitiesDto(attractionDtos, entity.getApartmentFacilities());
+    }
+*/
+    // since Java 8 style - more preferred
     @Override
     public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
-        return null;
+        var attractionsDtos = entity.getAttractions()
+                .stream()
+                .map(attraction -> attractionConverter.fromEntityToDto(attraction))
+                .toList();
+
+        return new HotelFacilitiesDto(attractionsDtos, entity.getApartmentFacilities());
     }
 
     @Override
     public HotelFacilities fromDtoToEntity(HotelFacilitiesDto dto) {
-        return null;
+
+        var entities = dto.attractions()
+                .stream()
+                .map(attractionDto -> attractionConverter.fromDtoToEntity(attractionDto))
+                .toList();
+
+        return new HotelFacilities(entities, dto.apartmentFacilities());
     }
 }

b526d29ec31f6908963f26834e4644fb6dc02493
pveelaid <pveelaid@gmail.com>
2022-09-04 13:39:01 +0300

    [fix] - trusting angular frontend application - no CORS issue anymore?!

--- backend/src/main/java/com/sda/travelagency/controller/TripController.java
+++ backend/src/main/java/com/sda/travelagency/controller/TripController.java
@@ -3,13 +3,12 @@ package com.sda.travelagency.controller;
 import com.sda.travelagency.entity.Trip;
 import com.sda.travelagency.service.TripService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO: fix CORS in better way
+@CrossOrigin("http://localhost:4200/")
 @RestController
 @Slf4j
 @RequestMapping("/api/trips")
